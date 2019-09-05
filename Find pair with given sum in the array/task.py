e31bfb584459a0430f70baaf0514b97bc736fe28
vitoOmbero <victor@gamedeveloper.by>
2019-09-05 10:04:32 +0300

    add pyhton dummy task

---
+++ Find pair with given sum in the array/task.py
@@ -0,0 +1,12 @@
+print('So... it works\r\n?')
+
+"""
+Input
+    arr = [9, 1, 5, 7, 2, 5, 9, 1, 2, 3, 6, 11]
+    sum = 10
+
+Output:
+    Pair found at index 0 and 1: (9 + 1)
+    or
+    Pair found at index 2 and 5: (5 + 5)
+"""
